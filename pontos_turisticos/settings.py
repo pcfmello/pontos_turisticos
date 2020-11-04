194827bb20bef4c3dde081d02cf92899c07e1239
Paulo Mello <pcfmello@gmail.com>
2020-11-03 22:34:24 -0300

    Criadas aplicações de comentários e avaliações

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
-    'atracoes'
+    'atracoes',
+    'comentarios',
+    'avaliacoes'
 ]
 
 MIDDLEWARE = [
